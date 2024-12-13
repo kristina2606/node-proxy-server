104687c890ebb01931813bd3f8ed5ee09a5086b5
Krystsina Labada <126324026+kristina2606@users.noreply.github.com>
2024-12-13 08:28:37 +0100

    Task-12: Add validation (#13)
    Co-authored-by: Kristina Labada <k.labada@godeltech.com>

--- routes/rovers-routes.js
+++ routes/rovers-routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { getRoverPhoto } = require("../controllers/rovers.controller");
+const validationHandler = require("../middleware/validationHandler");
+const roverSchema = require("../validationsSchemas/rovers.schema");
 
 const router = express.Router();
 
-router.post("/rover-photo", getRoverPhoto);
+router.post("/rover-photo", validationHandler(roverSchema), getRoverPhoto);
 
 module.exports = router;
